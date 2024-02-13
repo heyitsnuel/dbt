e36b529323b8266e7af9b5f42b634b9b62fe7ee1
immanuel.ambhara@gmail.com <immanuel.ambhara@gmail.com>
2024-02-13 15:39:14 +0000

    update margin query

--- models/challenge/intermediate/int_sales_margin.sql
+++ models/challenge/intermediate/int_sales_margin.sql
@@ -5,7 +5,7 @@ select
     revenue,
     quantity,
     purchase_price,
-    revenue-cast(purchase_price as float64) as margin
+    revenue - cast(purchase_price as float64) as margin
 from {{ ref('stg_raw__sales') }}
 left join {{ ref('stg_raw__product') }}
 using (products_id)
