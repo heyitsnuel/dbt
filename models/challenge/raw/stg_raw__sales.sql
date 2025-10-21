33670891e8011663e1b259658e7a23ced135313c
Immanuel Ambhara <immanuel.ambhara@gmail.com>
2025-10-21 08:35:35 +0000

    reset repo

--- models/challenge/raw/stg_raw__sales.sql
+++
@@ -1,7 +0,0 @@
-select
-    date_date,
-    orders_id,
-    pdt_id as products_id,
-    revenue,
-    quantity
-from {{ source('raw', 'sales') }}
--- models/sales.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  date_date,
-  orders_id,
-  products_id,
-  customers_id,
-  turnover
-FROM `civic-genius-328315.lewagon_data_analytics.gwz_sales`
